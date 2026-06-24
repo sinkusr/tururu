d2f7875a7fd11ea2e63cf426142593c676738919
sinkusr <55919565+sinkusr@users.noreply.github.com>
2026-06-24 16:10:34 +0900

    Delete count.ps1

--- count.ps1
+++
@@ -1,7 +0,0 @@
-$c = Get-Content app.js -Raw
-$o = [regex]::Matches($c, '\{').Count
-$cl = [regex]::Matches($c, '\}').Count
-$po = [regex]::Matches($c, '\(').Count
-$pcl = [regex]::Matches($c, '\)').Count
-Write-Host "Curly Open: $o, Close: $cl"
-Write-Host "Parens Open: $po, Close: $pcl"
